0c993440dc2e4c426bec6dc6a4a2bbab610f1431
Tolja Zubow <zubow@tkn.tu-berlin.de>
2016-11-24 13:01:22 +0100

    Cleanup.

--- sbi/protocols/net_filter.py
+++ sbi/protocols/net_filter.py
@@ -1,9 +1,9 @@
 from sbi.common.protocol import Protocol
 
-__author__ = "Piotr Gawlowicz, Anatolij Zubow, Mikolaj Chwalisz"
+__author__ = "Piotr Gawlowicz, Anatolij Zubow"
 __copyright__ = "Copyright (c) 2015, Technische Universitat Berlin"
 __version__ = "0.1.0"
-__email__ = "{gawlowicz, zubow, chwalisz}@tkn.tu-berlin.de"
+__email__ = "{gawlowicz, zubow}@tkn.tu-berlin.de"
 
 
 class NetFilter(Protocol):
